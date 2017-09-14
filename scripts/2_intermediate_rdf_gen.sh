7d89b42245024c736cddf920052bf988caec5b9a
bill-baumgartner <bill.baumgartner@gmail.com>
2017-09-13 23:54:17 -0600

    S

--- scripts/2_intermediate_rdf_gen.sh
+++ scripts/2_intermediate_rdf_gen.sh
@@ -5,4 +5,4 @@
 #
 
 # run the pharmgkb genes r2rml
-docker run --rm --net carnival-net --volumes-from carnival_data billbaumgartner/carnival-r2rml:0.1 sh -c '/carnival_scripts/run_r2rml.sh -c /carnival_config/pharmgkb/pharmgkb_genes.ttl'
+docker run --rm --net=carnival-net --volumes-from carnival_data billbaumgartner/carnival-r2rml:0.1 sh -c '/carnival_scripts/run_r2rml.sh -c /carnival_config/pharmgkb/pharmgkb_genes.ttl'
