3877784a2161cc2bf6283950fc91983f5f9717a3
bill-baumgartner <bill.baumgartner@gmail.com>
2017-09-13 22:46:47 -0600

    Initial commit of step 2 script (intermediate rdf gen)

---
+++ scripts/2_intermediate_rdf_gen.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#
+# NOTE: Apache Drill container must be running at this point
+#
+
+# run the pharmgkb genes r2rml
+docker run --rm --net carnival-net --volumes-from carnival_data billbaumgartner/carnival-r2rml:0.1 sh -c '/carnival_scripts/run_r2rml.sh -c /carnival_config/pharmgkb/pharmgkb_genes.ttl'
